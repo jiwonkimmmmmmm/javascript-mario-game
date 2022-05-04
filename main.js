4add42d0df8ca768ab68bd8c518022d90c75697f
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-05-04 16:32:29 +0900

    점프 애니메이션 추가

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ const c = canvas.getContext("2d");
 canvas.with = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const gravity = 0.5;
+
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 20,
     };
     this.width = 50;
     this.height = 50;
@@ -21,7 +27,25 @@ class Player {
     c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    // 가장 하단까지만 이동
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
 }
 
 const player = new Player();
-player.draw();
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+animate();
