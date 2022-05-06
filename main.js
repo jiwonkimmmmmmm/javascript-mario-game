92849fe762c2aba345719755fc3e6f002bc58085
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-05-06 16:13:51 +0900

    플레이어 좌우상하 이동 이벤트 추가

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const canvas = document.querySelector("canvas");
 
 const c = canvas.getContext("2d");
 
-canvas.with = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const gravity = 0.5;
 
@@ -30,6 +30,7 @@ class Player {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     // 가장 하단까지만 이동
@@ -42,10 +43,68 @@ class Player {
 }
 
 const player = new Player();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
 }
+
+addEventListener("keydown", ({ keyCode }) => {
+  console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      keys.left.pressed = true;
+      break;
+    case 83:
+      console.log("down");
+      break;
+    case 68:
+      console.log("right");
+      keys.right.pressed = true;
+      break;
+    case 87:
+      console.log("up");
+      player.velocity.y -= 15;
+      break;
+  }
+});
+
+addEventListener("keyup", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      keys.left.pressed = false;
+      break;
+    case 83:
+      console.log("down");
+      break;
+    case 68:
+      console.log("right");
+      keys.right.pressed = false;
+      break;
+    case 87:
+      console.log("up");
+      // player.velocity.y -= 10;
+      break;
+  }
+});
+
 animate();
