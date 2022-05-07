18e02f045bf6d3df54a0ff41108f00c036afc7c4
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-05-07 23:20:58 +0900

    x좌표에 따른 플랫폼 이동

--- main.js
+++ main.js
@@ -43,10 +43,10 @@ class Player {
 }
 
 class Platform {
-  constructor() {
+  constructor({ x, y }) {
     this.position = {
-      x: 200,
-      y: 600,
+      x,
+      y,
     };
     this.width = 200;
     this.height = 20;
@@ -59,7 +59,11 @@ class Platform {
 }
 
 const player = new Player();
-const platform = new Platform();
+
+const platforms = [
+  new Platform({ x: 200, y: 600 }),
+  new Platform({ x: 400, y: 700 }),
+];
 
 const keys = {
   right: {
@@ -74,26 +78,40 @@ function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
-  platform.draw();
+  platforms.forEach((platform) => {
+    platform.draw();
+  });
 
-  if (keys.right.pressed) {
+  // 플레이어 x좌표 이동 설정
+  if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5;
-  } else if (keys.left.pressed) {
+  } else if (keys.left.pressed && player.position.x > 100) {
     player.velocity.x = -5;
   } else {
     player.velocity.x = 0;
+    if (keys.right.pressed) {
+      platforms.forEach((platform) => {
+        platform.position.x -= 5;
+      });
+    } else if (keys.left.pressed) {
+      platforms.forEach((platform) => {
+        platform.position.x += 5;
+      });
+    }
   }
 
   // 플랫폼과 충돌 시
-  if (
-    player.position.y + player.height <= platform.position.y &&
-    player.position.y + player.height + player.velocity.y >=
-      platform.position.y &&
-    player.position.x + player.width >= platform.position.x &&
-    player.position.x <= platform.position.x + platform.width
-  ) {
-    player.velocity.y = 0;
-  }
+  platforms.forEach((platform) => {
+    if (
+      player.position.y + player.height <= platform.position.y &&
+      player.position.y + player.height + player.velocity.y >=
+        platform.position.y &&
+      player.position.x + player.width >= platform.position.x &&
+      player.position.x <= platform.position.x + platform.width
+    ) {
+      player.velocity.y = 0;
+    }
+  });
 }
 
 addEventListener("keydown", ({ keyCode }) => {
